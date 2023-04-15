71df073919d3e279175433ac52426c1d9906a3e9
jaehunkim <jaehunkim@outlook.com>
2023-04-15 21:57:14 +0900

    tailwind configs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    experimental: {
+        applyComplexClasses: true,
+        uniformColorPalette: true,
+        extendedFontSizeScale: true,
+    },
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto", "Noto Sans", "Arial"],
+                pop: ["Poppins", "Noto Sans", "Arial"],
+                sans: ["Noto Sans", "Roboto", "Arial"],
+            },
+        },
+    },
+    plugins: [],
+};
